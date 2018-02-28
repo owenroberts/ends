db30cb718ba67b5f4b258c1d5dfba1b501c8486d
owenroberts <obr208@nyu.edu>
2018-02-27 23:23:47 -0500

    updates birds part 22

---
+++ update.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+CAT=$1
+PART=$2
+TITLE=$3
+DATE=`date +%Y-%m-%d`
+
+# move json anim file from downloads to current project folder d
+mv ~/Downloads/$PART.json assets/$CAT/$PART.json
+
+# make html file
+HTML="_posts/$CAT/$DATE-$CAT-$PART.html"
+touch $HTML 
+
+cat > $HTML << EOF
+---
+layout: post
+title: $TITLE
+part: $PART
+date: $DATE
+categories: $CAT
+---
+<canvas 
+	id="lines" 
+	data-src="{{ site.url }}{{ site.baseurl }}/assets/{{ page.categories.first }}/$PART.json"
+></canvas>
+{% include post_script.html %}
+EOF
+
+# publish to lines.owen.cool
+open http://localhost:4000
+echo "Publish? y/n"
+read PUBLISH
+
+if [ "$PUBLISH" = "y" ] ; then
+   	git add .
+   	git commit -m "updates $CAT part $PART"
+   	git push origin master
+else
+    exit
+fi
+
+# make gif
+# convert -delay 10 -loop 0 ~/Downloads/$PART*.png social/$CAT/$CAT-$DATE.gif
+
+#make mp4
+# ffmpeg -ignore_loop 0 -i social/$CAT/$CAT-$DATE.gif -c:v libx264 -pix_fmt yuv420p -crf 4 -b:v 300K -vf scale=640:-1 -t 4 -movflags +faststart social/$CAT/$CAT-$DATE.mp4
+
+# post on twitter (?)
+
+# post on insta (?)
